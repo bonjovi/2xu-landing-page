3a8cff5875e717c14c1d17b725790b0bab659ee8
Roman Kalashnikov <rk@MacBook-Pro-Roman.local>
2017-11-13 11:26:21 +0300

    Added gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,20 @@
 var gulp = require('gulp'),
-    sass = require('gulp-sass'); 
+    sass = require('gulp-sass'), 
+    concatcss = require('gulp-concat-css');
+
+gulp.task('blocks', function() {
+    return gulp.src('app/scss/blocks/**/*.scss')
+    	.pipe(concatcss("_blocks.scss"))
+    	.pipe(gulp.dest('app/scss/blocks/'));
+});
 
 gulp.task('sass', function() {
     return gulp.src('app/scss/main.scss')
         .pipe(sass())
         .pipe(gulp.dest('app/css'))
+});
+
+gulp.task('watch', function() {
+    gulp.watch('app/sсss/**/*.sсss', ['blocks','sass']); // Наблюдение за sass файлами
+    // Наблюдение за другими типами файлов
 });
