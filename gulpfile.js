b182e4bb6c49dfd32192bb0a425a8f75bfdf0090
Roman Kalashnikov <roman-kalashnikoff@mail.ru>
2017-11-13 17:16:06 +0300

    Added header

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,32 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'), 
-    concatcss = require('gulp-concat-css');
+    concatcss = require('gulp-concat'),
+    clean = require('gulp-clean'),
+    plumber = require('gulp-plumber');
 
 gulp.task('blocks', function() {
     return gulp.src('app/scss/blocks/**/*.scss')
     	.pipe(concatcss("_blocks.scss"))
-    	.pipe(gulp.dest('app/scss/blocks/'));
+        .pipe(plumber())
+    	.pipe(gulp.dest('app/scss/build/'));
 });
 
 gulp.task('sass', function() {
     return gulp.src('app/scss/main.scss')
         .pipe(sass())
+        .pipe(plumber())
         .pipe(gulp.dest('app/css'))
 });
 
+gulp.task('clean', function () {  
+    return gulp.src('app/scss/blocks/_blocks.scss', {read: false})
+        .pipe(clean());
+});
+
 gulp.task('watch', function() {
-    gulp.watch('app/sсss/**/*.sсss', ['blocks','sass']); // Наблюдение за sass файлами
-    // Наблюдение за другими типами файлов
+    gulp.watch(['app/scss/**/*.scss', 'app/index.html'], ['blocks']);
+    setTimeout(function() {
+        gulp.watch(['app/scss/**/*.scss', 'app/index.html'], ['sass']);
+    }, 1000);
+    //gulp.watch('app/scss/**/*.scss', ['blocks']);
 });
