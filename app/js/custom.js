76fbb9d3089784400f83b9d9b92d847efe9ce5ba
Roman Kalashnikov <roman-kalashnikoff@mail.ru>
2017-11-15 13:06:16 +0300

    Added feedback block

--- app/js/custom.js
+++ app/js/custom.js
@@ -5,4 +5,18 @@ $(function() {
 		loop: true,
 		nav: true
 	});
+
+	
 });
+
+function getName (str) {
+    if(str.lastIndexOf('\\')) {
+        var i = str.lastIndexOf('\\')+1;
+    }
+    else {
+        var i = str.lastIndexOf('/')+1;
+    }						
+    var filename = str.slice(i);			
+    var uploaded = document.getElementById("feedback__attachedfilename");
+    uploaded.innerHTML = filename;
+}
